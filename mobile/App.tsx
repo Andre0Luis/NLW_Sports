ec3b5ce76605c4bc179cd130d6927a52a19eec60
andre.teixeira <andre.teixeira@fncit.com.br>
2022-09-13 18:27:13 -0300

    [ALT] Aula 1 FInalizada

--- mobile/App.tsx
+++ mobile/App.tsx
@@ -1,20 +1,48 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, TouchableOpacity, View } from 'react-native';
+
+interface ButtonProps {
+  title: string;
+}
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <StatusBar style="auto" backgroundColor='blue' />
+      <Text style={styles.title}>Meu appzão!</Text>
+      <Text style={styles.title}>Aperte na Abobrinha</Text>
+      <Button title='Abobrinha'></Button>
+      <Button title='Abobrinha'></Button>
+      <Button title='Abobrinha'></Button>
     </View>
   );
+}
+
+
+function Button(props: ButtonProps) {
+  return (
+    <TouchableOpacity>
+      <Text style={styles.fontButton} >
+        {props.title}
+      </Text>
+    </TouchableOpacity>
+  )
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: '#000000',
     alignItems: 'center',
     justifyContent: 'center',
   },
+  title: {
+    color: '#ffffff',
+    fontSize: 18
+  },
+  fontButton: {
+    color: '#ffffff',
+    fontSize: 14,
+    padding: 20
+  }
 });
