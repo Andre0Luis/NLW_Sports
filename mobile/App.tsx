bb705082dcb84e136f7d53522e864754dbb377e0
andre.teixeira <andre.teixeira@fncit.com.br>
2022-09-15 15:12:04 -0300

    [ALT] mobile

--- mobile/App.tsx
+++ mobile/App.tsx
@@ -1,48 +1,24 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, TouchableOpacity, View } from 'react-native';
-
-interface ButtonProps {
-  title: string;
-}
+import { StatusBar } from "react-native";
+import { Background } from "./src/components/Background";
+import { useFonts, Inter_400Regular, Inter_600SemiBold, Inter_700Bold, Inter_900Black } from "@expo-google-fonts/inter";
+import { Home } from "./src/screens/Home";
+import { Loading } from "./src/components/Loading/loading";
 
 export default function App() {
+  const [fontsLoaded] = useFonts({
+    Inter_400Regular, 
+    Inter_600SemiBold, 
+    Inter_700Bold, 
+    Inter_900Black
+  })
+
   return (
-    <View style={styles.container}>
-      <StatusBar style="auto" backgroundColor='blue' />
-      <Text style={styles.title}>Meu appzão!</Text>
-      <Text style={styles.title}>Aperte na Abobrinha</Text>
-      <Button title='Abobrinha'></Button>
-      <Button title='Abobrinha'></Button>
-      <Button title='Abobrinha'></Button>
-    </View>
+    <Background>
+      <StatusBar 
+      barStyle="light-content"
+      backgroundColor="transparent"
+      translucent/>
+      { fontsLoaded ?  <Home /> : <Loading /> }
+    </Background>
   );
 }
-
-
-function Button(props: ButtonProps) {
-  return (
-    <TouchableOpacity>
-      <Text style={styles.fontButton} >
-        {props.title}
-      </Text>
-    </TouchableOpacity>
-  )
-}
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#000000',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-  title: {
-    color: '#ffffff',
-    fontSize: 18
-  },
-  fontButton: {
-    color: '#ffffff',
-    fontSize: 14,
-    padding: 20
-  }
-});
