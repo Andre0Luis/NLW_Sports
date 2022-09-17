e007cb0b78a2623aeec6bcc57f036fb347515967
Andre0Luis <andre_luis.t@hotmail.com>
2022-09-16 21:39:59 -0300

    [ALT] WEB react componentizado consumindo jogos

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -1,82 +1,62 @@
-import { MagnifyingGlassPlus } from 'phosphor-react';
 import './styles/main.css';
+import { useState, useEffect } from 'react';
 import logoImg from './assets/logo.svg';
+import { GameBanner } from './components/GameBanner';
+import { CreateAdBanner } from './components/CreateAdBanner';
+
+interface Game {
+  id: string;
+  title: string;
+  bannerUrl: string;
+  _count: {
+    ads: number;
+  }
+}
 
 function App() {
+  //Tudo que for colocado aqui vai funcionar no momento em que for exibito em tela
+
+  //Colocamos o Game[] para definir o tipo do objeto que será recebido
+  const [games, setGames] = useState<Game[]>([]);
+
+  //Hook de efeitos colaterais, ele somente vai executar o que estiver dentro dele
+  useEffect(() => {
+    fetch('http://localhost:7777/games')
+      .then(response => response.json())
+      .then(data => {
+        setGames(data)
+      })
+  }, []);
+  //Se eu não passar nenhuma váriavel no array, ele vai executar apenas uma vez na função.
+
+
   return (
     <div className='max-w-[1344px] mx-auto flex flex-col items-center my-20'>
-    <img src={logoImg} alt="logo NLW eSports"/>
+      <img src={logoImg} alt="logo NLW eSports" />
       <h1 className='text-6xl text-white font-black mt-20'>
         Seu <span className='text-transparent bg-nlw-gradient bg-clip-text'>duo</span> está aqui!
       </h1>
 
+      { /*  Listagem dos Banners de jogos 
+            Sempre que for passar uma lista de coisas colocar um key no elemento 
+      */}
       <div className='grid grid-cols-6 gap-6 mt-16'>
-        <a href="" className='relative rounded-lg overflow-hidden '>
-          <img src="1.png" alt="" />
-          <div className='w-full pt-16 pb-4 px-4 bg-game-gradient absolute bottom-0 left-0 right-0'>
-            <strong className='font-bold text-white block'>League of Legends</strong>
-            <span className='text-zinc-300 text-sm block'>4 anúncios</span>
-          </div>
-        </a>
-        <a href="" className='relative rounded-lg overflow-hidden'>
-          <img src="2.png" alt="" />
-          <div className='w-full pt-16 pb-4 px-4 bg-game-gradient absolute bottom-0 left-0 right-0'>
-            <strong className='font-bold text-white block'>League of Legends</strong>
-            <span className='text-zinc-300 text-sm block'>4 anúncios</span>
-          </div>
-        </a>
-        <a href="" className='relative rounded-lg overflow-hidden'>
-          <img src="3.png" alt="" />
-          <div className='w-full pt-16 pb-4 px-4 bg-game-gradient absolute bottom-0 left-0 right-0'>
-            <strong className='font-bold text-white block'>League of Legends</strong>
-            <span className='text-zinc-300 text-sm block'>4 anúncios</span>
-          </div>
-        </a>
-        <a href="" className='relative rounded-lg overflow-hidden'>
-          <img src="4.png" alt="" />
-          <div className='w-full pt-16 pb-4 px-4 bg-game-gradient absolute bottom-0 left-0 right-0'>
-            <strong className='font-bold text-white block'>League of Legends</strong>
-            <span className='text-zinc-300 text-sm block'>4 anúncios</span>
-          </div>
-        </a>
-        <a href="" className='relative rounded-lg overflow-hidden'>
-          <img src="5.png" alt="" />
-          <div className='w-full pt-16 pb-4 px-4 bg-game-gradient absolute bottom-0 left-0 right-0'>
-            <strong className='font-bold text-white block'>League of Legends</strong>
-            <span className='text-zinc-300 text-sm block'>4 anúncios</span>
-          </div>
-        </a>
-        <a href="" className='relative rounded-lg overflow-hidden'>
-          <img src="6.png" alt="" />
-          <div className='w-full pt-16 pb-4 px-4 bg-game-gradient absolute bottom-0 left-0 right-0'>
-            <strong className='font-bold text-white block'>League of Legends</strong>
-            <span className='text-zinc-300 text-sm block'>4 anúncios</span>
-          </div>
-        </a>
+        {games.map(game => {
+          return (
+            <GameBanner
+              key={game.id}
+              bannerUrl={game.bannerUrl}
+              title={game.title}
+              adsCount={game._count.ads} />
+          )
+        })}
       </div>
 
-      <div className='pt-2 bg-nlw-gradient mt-8 self-stretch rounded-lg overflow-hidden'>
-        <div className='bg-[#2A2634] px-8 py-6 flex justify-between items-center'>
-          
-          <div>
-            <strong className='text-2xl text-white font-black block'>Não encontrou seu duo?</strong>
-            <span className='text-zinc-400 block'>Publique um anúncio para encontrar novos players!</span>
-          </div>
-
-          <button className='py-3 px-4 bg-violet-500 hover:bg-violet-700 text-white rounded flex items-center gap-3'>
-            <MagnifyingGlassPlus size={24} />
-            Publicar anúncio
-          </button>
-        </div>
-      </div>
-  </div>
+      { /* Banner de publicação de anúncios */}
+      <CreateAdBanner />
+    </div>
   )
-}
+};
 
-// function Button(props: ButtonProps){
-//   return (
-//     <button> {props.title} </button>
-//   )
-// }
 
 export default App
